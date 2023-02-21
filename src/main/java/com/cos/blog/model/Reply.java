f2e163a8e6b7c2cfe5ec32bda3834c0065969398
winone <100winone@naver.com>
2023-02-21 22:17:27 +0900

    2023.02.21 JPA 테이블 생성 - 2

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,4 +1,35 @@
 package com.cos.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
 public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne // 여러개의 댓글은 하나의 게시글에 존재할 수 있음
+    @JoinColumn(name="boardId")
+    private Board board;
+
+    @ManyToOne // 여러개의 답글을 하나의 유저가 쓸 수 있음
+    @JoinColumn(name="userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -1,10 +1,19 @@
 package com.cos.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴
 // ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity  // User클래스가 MySql에 테이블이 생성이 된다.
 public class User {
@@ -12,6 +21,7 @@ public class User {
     @Id // PK가 됨
     @GeneratedValue(strategy = GenerationType.IDENTITY) // numbering 전략. 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
     private int id; // 시퀀스, auto_increment
+
     @Column(nullable = false, length = 30)
     private String username; // 아이디
 
