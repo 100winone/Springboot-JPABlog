53582371333cc3105fac3b4f76421260443e48f7
winone <100winone@naver.com>
2023-03-07 00:21:34 +0900

    2023.03.06 기본 화면 구성

---
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.cos.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping({"","/"}) // 아무것도 안붙인 경우와 / 붙인 경우 index를 찾아감
+    public String index() {
+        return "index";
+    }
+}
